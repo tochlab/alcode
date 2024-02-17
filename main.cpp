976a5b68fb98c91c1615048491926bc21b39969c
Dmitriy Tochanskiy <d.tochanskiy@yadro.com>
2024-02-17 19:40:05 +0300

    main.cpp: Open file and load to editor

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <fstream>
 
 #include <FL/Fl.H>
 #include <FL/Fl_Window.H>
@@ -7,6 +8,7 @@
 #include <FL/Fl_Text_Buffer.H>
 #include <FL/Fl_Menu_Bar.H>
 #include <FL/Fl_Tile.H>
+#include <FL/Fl_File_Chooser.H>
 
 class FixedHeightMenuBar : public Fl_Menu_Bar {
 
@@ -23,10 +25,24 @@ public:
 
 class MainWindow : public Fl_Window {
 private:
+
+    Fl_Text_Buffer tb;
+
     static void quit_callback(Fl_Widget *, void *data) {
         std::cout << "Quit" << std::endl;
         auto mw = (MainWindow *) data;
         mw->hide();
+    }
+
+    static void open_callback(Fl_Widget* w, void* data) {
+    	std::cout << "Open" << std::endl;
+	auto mw = (MainWindow* ) data;
+	auto file_name  = fl_file_chooser("Open...", "*", NULL);
+	if (file_name == nullptr)
+		return;
+
+	std::cout << "Choosen " << file_name << std::endl;
+	mw->tb.loadfile(file_name);
     }
 
 public:
@@ -35,12 +51,12 @@ public:
 
         auto menu_bar = new FixedHeightMenuBar(0, 0, this->w(), 25);
         menu_bar->add("File/Quit", FL_COMMAND + 'q', quit_callback, this);
+	menu_bar->add("File/Open", FL_COMMAND + 'o', open_callback, this);
 
         auto file_tree = new Fl_Tree(0, menu_bar->h(), 200, this->h());
 
         auto text_editor = new Fl_Text_Editor(file_tree->w(), menu_bar->h(), this->w() - file_tree->w(), this->h());
-        auto text_buffer = new Fl_Text_Buffer();
-        text_editor->buffer(text_buffer);
+        text_editor->buffer(tb);
         this->end();
 
         resizable(this);
